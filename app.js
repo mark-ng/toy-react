d5b900fc4e96e31b364a898aa8056471ba5baa4d
markng <stngad@connect.ust.hk>
2024-01-12 04:55:04 +0800

    Update the virtualDOM from array to object

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-// Single source of truth
+// states
 let name = "";
 let count = 0;
 
-function createVDOM() {
-  return [
-    ["input", name, handle],
-    ["div", `Hello, ${name}`],
-    ["div", "Great job!"],
-    ["button", "+", null, increment],
-    ["div", `Count: ${count}`],
-  ];
-}
-
+// update state functions
 function handle(e) {
   name = e.target.value;
 }
@@ -20,31 +11,96 @@ function increment() {
   count += 1;
 }
 
+function createVDOM() {
+  return {
+    ele: "div",
+    value: undefined,
+    textContent: undefined,
+    childs: [
+      {
+        ele: "input",
+        value: name,
+        textContent: undefined,
+        oninput: handle,
+        childs: [],
+      },
+      {
+        ele: "div",
+        value: undefined,
+        textContent: `Hello , ${name}`,
+        oninput: undefined,
+        childs: [],
+      },
+      {
+        ele: "div",
+        value: undefined,
+        textContent: `Should never update`,
+        oninput: undefined,
+        childs: [],
+      },
+      {
+        ele: "button",
+        value: undefined,
+        textContent: "+",
+        onclick: increment,
+        childs: [],
+      },
+      {
+        ele: "div",
+        value: undefined,
+        textContent: `Count: ${count}`,
+        childs: [],
+      },
+    ],
+  };
+}
+
 // Framework
 
-let curVDOM;
+let currVDOM;
 let prevVDOM;
 let elems;
 
-function findDiff(prev, current) {
-  for (let i = 0; i < current.length; i++) {
-    if (JSON.stringify(prev[i]) !== JSON.stringify(current[i])) {
-      elems[i].textContent = current[i][1];
-      elems[i].value = current[i][1];
+// Description: Diffing algorithm for virtualDOM
+// Implementation: Recersively traverse through the previous virtualDOM and current virtualDOM,
+// modify the element in actualDOM only when there is a difference on the go
+function findDiff(prev, curr, elems) {
+  const {childs: prevChilds, ...prevProperties} = prev;
+  const {childs: currChilds, ...currProperties} = curr;
+  if (JSON.stringify(prevProperties) !== JSON.stringify(currProperties)) {
+    /* ASSUMPTION ALERT: the component properties from the virtualDOM 
+      have the same name as the properties from the actualDOM */
+    for (let key of Object.keys(currProperties)) {
+      elems[key] = currProperties[key];
     }
+  }
+  for (let i = 0; i < prevChilds.length; i++) {
+    findDiff(prevChilds[i], currChilds[i], elems.childNodes[i]);
   }
 }
 
 function updateDOM() {
   if (elems == undefined) {
-    curVDOM = createVDOM();
-    elems = curVDOM.map(convert);
-    document.body.append(...elems);
+    // create the virtualDOM from states
+    currVDOM = createVDOM();
+    // convert the virtualDOM elements to actualDOM elements
+    elems = dfsConvert(currVDOM);
+    // mounting the actualDOM elements to the actualDOM
+    document.body.append(elems);
   } else {
-    prevVDOM = [...curVDOM];
-    curVDOM = createVDOM();
-    findDiff(prevVDOM, curVDOM);
+    // Save the current virtualDOM for diffing algorithm
+    prevVDOM = JSON.parse(JSON.stringify(currVDOM));
+    currVDOM = createVDOM();
+    findDiff(prevVDOM, currVDOM, elems);
   }
+}
+
+function dfsConvert(root) {
+  let ele = convert(root);
+  for (let child of root.childs) {
+    ele.appendChild(dfsConvert(child));
+  }
+  return ele;
 }
 
 /*
@@ -52,12 +108,12 @@ function updateDOM() {
  element to real DOM element
 */
 function convert(node) {
-  let ele = document.createElement(node[0]);
-  ele.textContent = node[1];
-  ele.value = node[1];
-  ele.oninput = node[2];
-  ele.onclick = node[3];
+  let ele = document.createElement(node.ele);
+  const {child, ...nodeProperties} = node;
+  for (let key of Object.keys(nodeProperties)) {
+    ele[key] = nodeProperties[key];
+  }
   return ele;
 }
 
-setInterval(updateDOM, 15);
+setInterval(updateDOM, 1000);
