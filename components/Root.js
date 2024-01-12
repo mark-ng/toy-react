045c24c459001b7ed0d6331594fd3adb01077ffb
markng <stngad@connect.ust.hk>
2024-01-13 04:02:28 +0800

    Add root page

--- components/Root.js
+++ components/Root.js
@@ -1,10 +1,47 @@
+import {useState} from "../react.js";
 import CounterDemo from "./CounterDemo.js";
 import ImgDemo from "./ImgDemo.js";
 import InputDemo from "./InputDemo.js";
+import ToggleDemo from "./ToggleDemo.js";
+
+const pages = [InputDemo, ImgDemo, ToggleDemo, CounterDemo];
 
 export default function Root() {
+  const [page, setPage] = useState(0);
+
   return {
     ele: "div",
-    childs: [InputDemo(), CounterDemo(), ImgDemo()],
+    childs: [
+      {
+        ele: "div",
+        childs: [
+          {
+            ele: "button",
+            textContent: "First",
+            onclick: () => setPage(0),
+            childs: [],
+          },
+          {
+            ele: "button",
+            textContent: "Second",
+            onclick: () => setPage(1),
+            childs: [],
+          },
+          {
+            ele: "button",
+            textContent: "Third",
+            onclick: () => setPage(2),
+            childs: [],
+          },
+          {
+            ele: "button",
+            textContent: "Fourth",
+            onclick: () => setPage(3),
+            childs: [],
+          },
+        ],
+      },
+      pages[page](),
+    ],
   };
 }
